e818753a6f102501805cca20ffd402ed2a196812
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-29 19:26:55 -0700

    created User Location Form with Semantic UI

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>User Location Page</h1>
+  <section class ="ui two column centered grid">
+    <div class="column">
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address" />
+              <i class="search icon"></i>
+            </div>
+          </div>
+          <button class="ui button">Click!</button>
+
+        </div>
+      </form>
+    </div>
+  </section>
 </template>
+
+<style>
+.ui.button,
+.dot.circle.icon {
+  background-color: #ff5a5f;
+  color: white;
+}
+</style>
