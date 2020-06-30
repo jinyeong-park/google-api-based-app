3744eecabd268e23896dc49c2e0b3c97bf0448ce
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-06-29 19:53:27 -0700

    connected Geolocation API

--- src/pages/UserLocation.vue
+++ src/pages/UserLocation.vue
@@ -7,16 +7,39 @@
           <div class="field">
             <div class="ui right icon input large">
               <input type="text" placeholder="Enter your address" />
-              <i class="search icon"></i>
+              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
             </div>
           </div>
-          <button class="ui button">Click!</button>
+          <button class="ui button"><i class="search icon"></i></button>
 
         </div>
       </form>
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  methods: {
+    locatorButtonPressed () {
+      // check whether geolocation API is supported in user's browser
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            console.log(position.coords.latitute)
+            console.log(position.coords.longitute)
+          },
+          error => {
+            console.log(error.message)
+          }
+        )
+      } else {
+        console.log("Your browser does not support geolocation API")
+      }
+    }
+  }
+}
+</script>
 
 <style>
 .ui.button,
