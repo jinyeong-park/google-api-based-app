43bff7dd1fe9ef8e7a557d406934215664b6e83c
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-07-02 15:12:04 -0700

    reconnected npm run start

--- e-commerce-market/src/App.js
+++ e-commerce-market/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import Homepage from './pages/homepage/Homepage';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-)
+import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
